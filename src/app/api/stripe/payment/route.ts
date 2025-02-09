081721c290704f4672d253e71746b7c0d2fe54dc
evregille <manu@metronome.com>
2025-02-09 07:22:17 -0800

    fixes

--- src/app/api/stripe/payment/route.ts
+++ src/app/api/stripe/payment/route.ts
@@ -1,4 +1,4 @@
-// import { NextResponse, NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 // import Stripe from "stripe";
 // let _stripe: Stripe | null = null;
@@ -11,9 +11,9 @@
 // };
 
 
-// export async function POST(req: NextRequest) {
-//   const { amount, captchaToken } = await req.json();
-
+export async function POST(req: NextRequest) {
+    const { amount } = await req.json();
+    console.log(amount)
 //   try {
 //     const response = await fetch(
 //       `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_CAPTCHA_SECRET_KEY}&response=${captchaToken}`, {
@@ -30,10 +30,10 @@
 //     }
 
 //     await getStripe().customers.create({email});
-//     return NextResponse.json({message: "Subscribed!"}, { status: 200 });
+    return NextResponse.json({message: "Subscribed!"}, { status: 200 });
 
 //   } catch (err) {
 //     console.log(err);
 //     return NextResponse.json({error: true, message: "Could not get you sign-up. Try later"}, { status: 400 });
 //   }
-// }
+}
