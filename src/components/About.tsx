559b4f5ea544ed82e33531ba91d20c637cced89c
evregille <manu@metronome.com>
2025-02-15 06:43:31 -0800

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,36 @@
 import { Tile } from "./ui/Tile";
+import Image from "next/image";
+import aboutImg from '../../public/about.png';
 
 export default function About() {
-    return (<div className="grid grid-cols-1 sm:grid-cols-1 gap-6 p-4 font-bold">
-        <Tile title="About">
-            <p>
-            Welcome to Lazy Bread! A small cottage bakery in the heart of the Piedmont neighborhood in North Portland. Ever since getting the hang of baking with sourdough about ten years ago, our family of 5 is stocked with a loaf of homemade bread at all times (and when we do run out, you can probably find an emergency loaf hiding in the freezer!). 
-            <br/>
-            <br/>    
-            Sourdough does take longer to rise than store-bought bread (I let mine go for at least 24 hours!), but I don’t believe that naturally risen bread with minimal ingredients is worth all the fuss it gets. Don’t get me wrong, it is damn delicious, has health benefits, and is oh-so-satisfying to make, but it isn’t a luxury product. 
-            <br/><br/>
-            My goal with Lazy Bread is to create delicious, unassuming bread at a more accessible price point. 
-            <br/><br/>
-            Sign up for my mailing list for updates on how and where to buy Lazy Bread! 
-            </p>
-        </Tile>
-    </div>);
+    return (
+        <div className="flex justify-center p-8">
+            <div className="w-[66.66%]">
+                <Tile title="About">
+                    <div className="flex gap-x-20">               
+                        <div className="flex-[1]">
+                            <p>
+                                Welcome to Lazy Bread! A small cottage bakery in the heart of the Piedmont neighborhood in North Portland. Ever since getting the hang of baking with sourdough about ten years ago, our family of 5 is stocked with a loaf of homemade bread at all times (and when we do run out, you can probably find an emergency loaf hiding in the freezer!). 
+                                <br/>
+                                <br/>    
+                                Sourdough does take longer to rise than store-bought bread (I let mine go for at least 24 hours!), but I don’t believe that naturally risen bread with minimal ingredients is worth all the fuss it gets. Don’t get me wrong, it is damn delicious, has health benefits, and is oh-so-satisfying to make, but it isn’t a luxury product. 
+                                <br/><br/>
+                                My goal with Lazy Bread is to create delicious, unassuming bread at a more accessible price point. 
+                                <br/><br/>
+                                Sign up for my mailing list for updates on how and where to buy Lazy Bread! 
+                            </p>
+                        </div>
+
+                        <div className="flex-[1]">
+                            <Image 
+                                src={aboutImg} 
+                                alt="Lazy Bread" 
+                                className=" object-cover rounded-lg" 
+                                width={400}
+                            />
+                        </div>
+                    </div>
+                </Tile>
+            </div>
+        </div>);
 }
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import { Tile } from './ui/Tile';
-
+import Image from 'next/image';
+import imgMarket from '../../public/contact-market.png'
+import imgCafe from '../../public/contact-cafe11.png';
 export default function Contact () {
   return (
-    <Tile
-      title={"Find Us"}
-    >
-      <div className="text-gray-700 mb-4 font-bold">
-    
-        <p>
-          Every weekends at Café Eleven (435 NE Rosa Parks, Portland OR 97211)
-        </p>
-        <br/>
-        <br/>
-        <p> 
-          Woodlawn Indoor Farmers Market every second Saturday from 11:00 - 2:00 ( March - May 2025). 
-        </p>
-      </div>
-    </Tile>
+    <div className="flex justify-center space-x-8 p-8">
+      <Tile title="">
+        <Image
+          src={imgCafe}
+          alt="cafe11"
+          className="w-100 h-40 object-cover rounded-t-lg"
+          width={420}
+          height={150}
+        />
+        <div className="mt-4 text-center">
+          <p className="text-gray-700 mb-4 font-bold">Café Eleven every weekend <br/> 435 NE Rosa Parks, Portland OR 97211</p>
+        </div>
+      </Tile>
+
+      <Tile title=''>
+        <Image
+          src={imgMarket}
+          alt="market"
+          className="w-100 h-40 object-cover rounded-t-lg"
+          width={420}
+          height={150}
+        />
+        <div className="mt-4 text-center">
+          <p className="text-gray-700 mb-4 font-bold">Woodlawn Farmers Market <br/>every second Saturday ( March - May 2025)</p>
+        </div>
+      </Tile>
+    </div>
   );
 };
