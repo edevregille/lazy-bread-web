6d3ea2ae368572fec843b193bbd0250cf3aec689
evregille <manu@metronome.com>
2025-02-14 21:45:40 -0800

    init

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,12 +4,14 @@ export default function About() {
     return (<div className="grid grid-cols-1 sm:grid-cols-1 gap-6 p-4 font-bold">
         <Tile title="About">
             <p>
-                Lazy Bread PDX is a small business in Portland, Oregon that specializes in sourdough focaccia bread. We are currently in the process of launching and will be offering a side of soup soon. 
-            </p>
+            Welcome to Lazy Bread! A small cottage bakery in the heart of the Piedmont neighborhood in North Portland. Ever since getting the hang of baking with sourdough about ten years ago, our family of 5 is stocked with a loaf of homemade bread at all times (and when we do run out, you can probably find an emergency loaf hiding in the freezer!). 
             <br/>
-            <br/>
-            <p>
-                Old fashioned bread made the lazy way: Minimal ingredients and naturally leavened. Feed it well and then leave to fatten up for 24 hours.
+            <br/>    
+            Sourdough does take longer to rise than store-bought bread (I let mine go for at least 24 hours!), but I don’t believe that naturally risen bread with minimal ingredients is worth all the fuss it gets. Don’t get me wrong, it is damn delicious, has health benefits, and is oh-so-satisfying to make, but it isn’t a luxury product. 
+            <br/><br/>
+            My goal with Lazy Bread is to create delicious, unassuming bread at a more accessible price point. 
+            <br/><br/>
+            Sign up for my mailing list for updates on how and where to buy Lazy Bread! 
             </p>
         </Tile>
     </div>);
--- src/components/ui/Tile.tsx
+++ src/components/ui/Tile.tsx
@@ -7,8 +7,8 @@ interface TitleProps {
 
 export const Tile: React.FC<TitleProps> = ({ title, children }) => {
     return (
-        <div className="p-6 bg-white rounded-lg shadow-lg shadow-[0px_-3px_12px_rgba(128,0,128,0.4)] w-full">
-            <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+        <div className="p-6 bg-white rounded-lg shadow-lg shadow-[0px_3px_8px_rgba(255,165,0,0.4),0px_-3px_8px_rgba(255,165,0,0.4),3px_0px_8px_rgba(255,165,0,0.4),-3px_0px_8px_rgba(255,165,0,0.4)] w-full">
+            <h2 className="text-2xl font-semibold mb-4 text-gradient_indigo-purple">{title}</h2>
             <div className="text-gray-700 mb-4">
                 {children}
             </div>
