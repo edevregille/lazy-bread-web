fdf7f0d561b7cf17536d76c34f86be81611316b5
MacBook <macbook@MACBOOKs-MacBook-Air.local>
2026-02-21 10:41:57 -0800

    fix header issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
 
     return (
         <>
-            <header className="fixed top-0 left-0 right-0 bg-white shadow-bakery z-10 border-b border-bakery-light">
+            <header className="fixed top-0 left-0 right-0 bg-white shadow-bakery z-10 border-t-0 border-b border-bakery-light">
             <div className="container mx-auto px-4">
                 <div className="flex items-center justify-between h-20">
                     {/* Logo */}
